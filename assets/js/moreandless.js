64db8ebccc9ff0eb396703ac3a2298bfe2dd36d4
Mr Kluch <karlkent36@gmail.com>
2017-06-16 05:45:19 +0000

    privet

---
+++ assets/js/moreandless.js
@@ -0,0 +1,49 @@
+// JavaScript File
+ document.getElementById('RefMore').style.display='inline';
+    document.getElementById('ArticleMore').style.display='none';
+
+    function ShowMore()
+    {
+      document.getElementById('ArticleMore').style.display='block';
+      document.getElementById('RefMore').style.display='none';
+      document.getElementById('RefLess').style.display='inline';
+    }
+
+    function ShowLess()
+    {
+      document.getElementById('ArticleMore').style.display='none';
+      document.getElementById('RefMore').style.display='inline';
+      document.getElementById('RefLess').style.display='none';
+    }
+     document.getElementById('RefMore2').style.display='inline';
+    document.getElementById('ArticleMore2').style.display='none';
+
+    function ShowMore2()
+    {
+      document.getElementById('ArticleMore2').style.display='block';
+      document.getElementById('RefMore2').style.display='none';
+      document.getElementById('RefLess2').style.display='inline';
+    }
+
+    function ShowLess2()
+    {
+      document.getElementById('ArticleMore2').style.display='none';
+      document.getElementById('RefMore2').style.display='inline';
+      document.getElementById('RefLess2').style.display='none';
+    }
+     document.getElementById('RefMore3').style.display='inline';
+    document.getElementById('ArticleMore3').style.display='none';
+
+    function ShowMore3()
+    {
+      document.getElementById('ArticleMore3').style.display='block';
+      document.getElementById('RefMore3').style.display='none';
+      document.getElementById('RefLess3').style.display='inline';
+    }
+
+    function ShowLess3()
+    {
+      document.getElementById('ArticleMore3').style.display='none';
+      document.getElementById('RefMore3').style.display='inline';
+      document.getElementById('RefLess3').style.display='none';
+    }
---
+++ assets/js/photo.js
@@ -0,0 +1,11 @@
+// JavaScript File
+function ShowBig(FileName)
+    {
+      document.getElementById('bigsize').style.display='block';
+      document.getElementById('BigImg').src=FileName;
+    }
+
+    function CloseBig()
+    {
+      document.getElementById('bigsize').style.display='none';
+    }
